5eedbd8888ee95f6f8c5dc977a8430a08645ca66
mudassirsayed <sayedmudassir37@gmail.com>
2020-09-21 18:44:10 +0530

    Updates

---
+++ precache-manifest.23fa1de6676178b0bf171e4ebf7f9a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a82880dc04b41a85102457b068b28e",
+    "url": "/searchMovie/index.html"
+  },
+  {
+    "revision": "22bc2119446c6264382a",
+    "url": "/searchMovie/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bd8e5bc100f57b4c5113",
+    "url": "/searchMovie/static/css/main.535c162a.chunk.css"
+  },
+  {
+    "revision": "22bc2119446c6264382a",
+    "url": "/searchMovie/static/js/2.334812b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/searchMovie/static/js/2.334812b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8e5bc100f57b4c5113",
+    "url": "/searchMovie/static/js/main.201c7740.chunk.js"
+  },
+  {
+    "revision": "786f4a91750e55a478d8",
+    "url": "/searchMovie/static/js/runtime-main.9561d984.js"
+  }
+]);
